012b74df71ca82411bf585043fe72678fc9d1bfa
Mohammed Jawad Sonalkar <mohammed.jawad.sonalkar@sap.com>
2018-07-19 21:02:59 +0530

    create user and feedback

--- public/js/utils.js
+++ public/js/utils.js
@@ -9,17 +9,15 @@
         messagingSenderId: "1035587128080"
     };
     firebase.initializeApp(config);
-    var isUserLogged = false, uid = ''; 
+    var isUserLogged = false, uid = '';
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            isUserLogged = true;
-            uid = user.uid;
+            loginSuccess();
         } else {
-            isUserLogged = false;
-            uid = '';
+            loggedFail();
         }
-      });
+    });
 
     $('#loginBtn').click(function (e) {
         var email = $('#loginEmail').val();
@@ -30,40 +28,87 @@
     $('#signupBtn').click(function (e) {
         var name = $('#signupName').val();
         var email = $('#signupEmail').val();
+        var password = "Zypnos123" + Math.floor(Math.random() * (1000000 - 99999) + 99999);
+
+        var oUserDetails = {
+            userName: name,
+            emailId: email,
+            userPassword: password
+        };
+        signUp(oUserDetails);
     });
 
     $('#feedbackBtn').click(function (e) {
         var name = $('#feedbackName').val();
         var email = $('#feedbackEmail').val();
         var message = $('#feedbackMessage').val();
+        var oFeedbackPayload = {
+            name: name,
+            email: email,
+            message: message,
+            time: getTimeStamp()
+        }
+        sendFeedback(oFeedbackPayload);
     });
 
-    loggedFail = function(){
-
+    loggedFail = function () {
+        isUserLogged = false;
+        uid = '';
     }
 
-    loginSuccess = function(){
-        window.location.href = "/recordNRun/index.html";
+    loginSuccess = function (user) {
+        isUserLogged = true;
+        uid = user.uid;
     }
 
-    login = function(email, pass){
+    login = function (email, pass) {
         firebase.auth().signInWithEmailAndPassword(email, pass)
-        .then(function (authData) {
-          if (firebase.auth() && firebase.auth().currentUser){
-            isUserLogged = true;
-            uid = firebase.auth().currentUser.uid;
-            loginSuccess();
-          }
-          else{
-            isUserLogged = false;
-            uid = '';
-            loggedFail();
-          }
-        })
-        .catch(function (error) {
-            isUserLogged = false;
-            uid = '';
-            loggedFail();
-        });
+            .then(function (authData) {
+                if (firebase.auth() && firebase.auth().currentUser) {
+                    loginSuccess(firebase.auth().currentUser);
+                }
+                else {
+                    loggedFail();
+                }
+            })
+            .catch(function (error) {
+                loggedFail();
+            });
     }
+
+    signUp = function (oUserDetails) {
+        firebase.auth().createUserWithEmailAndPassword(oUserDetails.emailId, oUserDetails.userPassword)
+            .then(function (user) {
+                firebase.database().ref(user.uid).child('user')
+                    .set(oUserDetails)
+                    .then(function () {
+
+                    });
+            })
+            .catch(function (error) {
+
+            });
+
+    };
+
+    sendFeedback = function (oFeedbackPayload) {
+        fSuccessHandler = function (oData) {
+        };
+        fErrorHandler = function (oData) {
+        };
+        firebase.database().ref('feedback').push(oFeedbackPayload)
+            .then(fSuccessHandler,fErrorHandler);
+    };
+
+    getTimeStamp = function () {
+        var time = new Date();
+        var year = time.getFullYear();
+        var month = time.getMonth() + 1;
+        var date = time.getDate();
+        var hour = time.getHours();
+        var minute = time.getMinutes();
+        var second = time.getSeconds();
+        var miliSecond = time.getMilliseconds();
+        return year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second + ":" + miliSecond;
+    };
 })();
